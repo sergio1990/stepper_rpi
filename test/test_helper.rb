57a6680c976823b0fbd12a47781d3c7e4a7f08de
Sergio Gernyak <sergeg1990@gmail.com>
2022-01-29 13:30:11 +0200

    Add mocha framework

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "stepper_rpi"
 
 require "minitest/autorun"
+require 'mocha/minitest'
 
 class DummyGpioAdapter < StepperRpi::GPIOAdapter
 end
